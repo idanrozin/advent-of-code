e3f76cc474e6760525c7ca92298478ef3457cf2d
Idan Rozin <idanrozin@gmail.com>
2021-01-01 21:19:50 +0200

    day 3 second question complete

--- Day3/trajectory.js
+++ Day3/trajectory.js
@@ -1,9 +1,17 @@
 import { getStringArrayFromInput } from '../utils/read-inputs.js';
-const RIGHT = 3;
+const TRAVERSE_DIRECTIONS = [ [ 1,1 ],[ 3,1 ],[ 5,1 ],[ 7,1 ],[ 1,2 ] ];
+
 const TREE = '#';
-const allTrees = rows => rows.map((row, i) => i !== 0 && row[(i * RIGHT) % row.length] === TREE).filter(item => item);
+const getTreesOnSlopeByDirection = (rows, right, down) => rows.map((row, i) => 
+    i !== 0 && 
+    i % down == 0 && 
+    row[((i / down) * right) % row.length] === TREE).filter(item => item);
 
 const data = getStringArrayFromInput('input.txt');
 const rows = data.map(row => row.split(''));
 
-console.log('allTrees', allTrees(rows).length)
+console.log('First Question:', getTreesOnSlopeByDirection(rows, TRAVERSE_DIRECTIONS[1][0], TRAVERSE_DIRECTIONS[1][1]).length);
+
+let product = 1 ;
+TRAVERSE_DIRECTIONS.forEach(direction => product *= getTreesOnSlopeByDirection(rows, direction[0], direction[1]).length);
+console.log('Second Question:', product)
