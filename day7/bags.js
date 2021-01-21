c41f2070430842e90c72b52d6ae6cefd60a1e55e
Idan Rozin <idan@browzwear.com>
2021-01-21 16:43:28 +0200

    attempt to start q2 of day7

--- day7/bags.js
+++ day7/bags.js
@@ -4,6 +4,7 @@ const utils = require('../utils/read-inputs.js');
 const BAG_COLOR_TO_CONTAIN = 'shiny gold bag';
 const PATTERN = "bags";
 const AllBags = [];
+const CONTAIN_TOKEN = "contain";
 const findBagsRecursive = (data, searchBags) => {
     const relevantBags = searchBags.map(currentBag => data.filter((rule, i) => {
         const canContain = rule.includes(currentBag) && !rule.startsWith(currentBag);
@@ -22,6 +23,27 @@ const findBagsRecursive = (data, searchBags) => {
     }
 };
 
+const getAllBagsInsideBag = (data, search) => {
+    const startingRule = data.find(rule => rule.startsWith(search));
+    // console.log("$$$ ~ file: bags.js ~ line 27 ~ startingRule", startingRule);
+    const allContainedBags = startingRule.substr(startingRule.indexOf(CONTAIN_TOKEN) + CONTAIN_TOKEN.length, startingRule.length).trim().split(', ');
+    allContainedBags.forEach(bag => {
+        let quantity = Number(bag.substr(0,bag.indexOf(" ")));
+        bag = bag.substr(bag.indexOf(" ") + 1);
+        if (isNaN(quantity)) {
+            return;
+        }
+
+        console.log("quantity: ", quantity);
+        console.log("bag:", bag);
+
+    });
+    
+}
+
 const data = utils.getStringArrayFromInput('input.txt');
-const allFitBags = findBagsRecursive(data, [ BAG_COLOR_TO_CONTAIN ]);
-console.log('Question 1 answer:', allFitBags);
+// const allFitBags = findBagsRecursive(data, [ BAG_COLOR_TO_CONTAIN ]);
+// console.log('Question 1 answer:', allFitBags);
+
+// const bagsInsideBag = getAllBagsInsideBag(data, BAG_COLOR_TO_CONTAIN);
+getAllBagsInsideBag(data, BAG_COLOR_TO_CONTAIN);
